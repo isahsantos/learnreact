f2c789a7939aa0c4eb442d4f0977c61ea997ec60
Isabela Santos <isa2014mgspn@gmail.com>
2020-06-01 14:28:57 -0300

    Firts commit

---
+++ fundamentosreact/src/index.js
@@ -0,0 +1,7 @@
+import './index.css'
+import Primeiro from './components/basics/First'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+
+ReactDOM.render( <div> < Primeiro > </Primeiro></div> , document.getElementById('root'))
