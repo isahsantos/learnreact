20b167d7512015576fc91ca7f1b3fb85d2d8d74d
Isabela Santos <isa2014mgspn@gmail.com>
2020-06-01 22:24:26 -0300

    Ajustes de exercicios com estilo css, números aleatorios, criação de componente

--- fundamentosreact/src/App.jsx
+++ fundamentosreact/src/App.jsx
@@ -2,18 +2,27 @@ import React, { Fragment } from 'react'
 import Primeiro from './components/basics/First'
 import ComponentJSX from './components/basics/CompnentJSX'
 import Aleatorio from './components/basics/Aleatorio'
-
+import Card from './components/layout/Card'
 
 export default () => (
     <Fragment>
-        < Primeiro > </Primeiro>
-        <Aleatorio min={1} max={100}></Aleatorio>
-        <ComponentJSX titulo="Auno" subtitulo="Situação do Aluno" ></ComponentJSX>
-        <ComponentJSX nome="Pedro" nota={9.3}></ComponentJSX>
-        <ComponentJSX
-            nome="Maria" nota={9.7}></ComponentJSX>
-        <ComponentJSX
-            nome="Caio" nota={5.7}></ComponentJSX>
+        <Card titulo="Primeiro Componente">
+            < Primeiro > </Primeiro>
+        </Card>
+        <Card titulo="Desafio Número Randomico">
+            <Aleatorio min={1} max={100}></Aleatorio>
+            <Aleatorio min={1} max={100}></Aleatorio>
+            <Aleatorio min={1} max={100}></Aleatorio>
+            <Aleatorio min={1} max={100}></Aleatorio>
+            <Aleatorio min={1} max={100}></Aleatorio>
+        </Card>
+        <Card titulo="Situação do Aluno">
+            <ComponentJSX nome="Pedro" nota={9.3}></ComponentJSX>
+            <ComponentJSX
+                nome="Maria" nota={9.7}></ComponentJSX>
+            <ComponentJSX
+                nome="Caio" nota={5.7}></ComponentJSX>
+        </Card>
     </Fragment>
 
 )
