f3a03972afd3963fa254262fab402391b3a935d3
Isabela Santos <isa2014mgspn@gmail.com>
2020-06-03 00:46:02 -0300

    Estilos  de card através de propriedades

--- fundamentosreact/src/App.jsx
+++ fundamentosreact/src/App.jsx
@@ -3,26 +3,33 @@ import Primeiro from './components/basics/First'
 import ComponentJSX from './components/basics/CompnentJSX'
 import Aleatorio from './components/basics/Aleatorio'
 import Card from './components/layout/Card'
+import './components/App.css'
 
 export default () => (
-    <Fragment>
-        <Card titulo="Primeiro Componente">
-            < Primeiro > </Primeiro>
-        </Card>
-        <Card titulo="Desafio Número Randomico">
+<div className="App">
+    <div className ="Cards">
+    <Card titulo=" #01 - Desafio Número Randomico">
             <Aleatorio min={1} max={100}></Aleatorio>
             <Aleatorio min={1} max={100}></Aleatorio>
             <Aleatorio min={1} max={100}></Aleatorio>
             <Aleatorio min={1} max={100}></Aleatorio>
             <Aleatorio min={1} max={100}></Aleatorio>
         </Card>
-        <Card titulo="Situação do Aluno">
+        <Card titulo="#02 - Desafio Número Randomico">
+            <Aleatorio min={1} max={100}></Aleatorio>
+            <Aleatorio min={1} max={100}></Aleatorio>
+            <Aleatorio min={1} max={100}></Aleatorio>
+            <Aleatorio min={1} max={100}></Aleatorio>
+            <Aleatorio min={1} max={100}></Aleatorio>
+        </Card>
+        <Card titulo="#03 - Situação do Aluno" color="#080">
             <ComponentJSX nome="Pedro" nota={9.3}></ComponentJSX>
             <ComponentJSX
                 nome="Maria" nota={9.7}></ComponentJSX>
             <ComponentJSX
                 nome="Caio" nota={5.7}></ComponentJSX>
         </Card>
-    </Fragment>
+        </div>
+        </div>
 
 )
